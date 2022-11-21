9fe6b94c76ef9573863fb8849ce7451994ee5e5c
Steven Khaw <swkhaw8226@yahoo.com>
2022-11-21 12:58:35 -0800

    fixed spacing

--- source/assets/scripts/JsonPopulateTest.js
+++ source/assets/scripts/JsonPopulateTest.js
@@ -5,47 +5,47 @@ window.addEventListener('DOMContentLoaded', init);
 
 // no comments since this one is just for test and will be removed in final product
 function init() {
-    // create a calendar object from hard-coded json
-    let calendarData = loadJson([testJson]);
-    // console.log(b);
+  // create a calendar object from hard-coded json
+  let calendarData = loadJson([testJson]);
+  // console.log(b);
 
-    // export calendar to json with different range of time selections
-    let tuesJson = calendarData[0].Export([2022,2023],[11],[1]);
-    let thursJson = calendarData[0].Export([-1],[11],[3]);
+  // export calendar to json with different range of time selections
+  let tuesJson = calendarData[0].Export([2022, 2023], [11], [1]);
+  let thursJson = calendarData[0].Export([-1], [11], [3]);
 
-    // create a calendar object from multiple json
-    calendarData = loadJson([tuesJson, thursJson]);
-    calendarData[0].Show(2022,11);
+  // create a calendar object from multiple json
+  calendarData = loadJson([tuesJson, thursJson]);
+  calendarData[0].Show(2022, 11);
 
-    // simple tests for reading/writing json from/to local drive
+  // simple tests for reading/writing json from/to local drive
 
-    // temporarily deprecated
-    // write json string to local drive
-    // const downloadBtn = document.getElementById('download-btn');
-    // downloadBtn.addEventListener('click', e => {
-    //     download(tuesJson);
-    // });
+  // temporarily deprecated
+  // write json string to local drive
+  // const downloadBtn = document.getElementById('download-btn');
+  // downloadBtn.addEventListener('click', e => {
+  //     download(tuesJson);
+  // });
 
-    // read local drive
-    const uploadBtn = document.getElementById('upload-btn');
-    uploadBtn.addEventListener('click', e => {
-        const fileBtn = document.getElementById('files');
-        fileBtn.click();
-    });
-
-    // select local drive 
+  // read local drive
+  const uploadBtn = document.getElementById('upload-btn');
+  uploadBtn.addEventListener('click', e => {
     const fileBtn = document.getElementById('files');
-    fileBtn.addEventListener('change',async e =>    {
-        const [file] = e.target.files
+    fileBtn.click();
+  });
 
-        if(!file) {
-                console.log('no file');
-        }
-        else {
-                const data = await file.text();
-                console.log(data);
-        }
-    });
+  // select local drive 
+  const fileBtn = document.getElementById('files');
+  fileBtn.addEventListener('change', async e => {
+    const [file] = e.target.files
+
+    if (!file) {
+      console.log('no file');
+    }
+    else {
+      const data = await file.text();
+      console.log(data);
+    }
+  });
 
 
 }
--- source/assets/scripts/jsonInterpreter.js
+++ source/assets/scripts/jsonInterpreter.js
@@ -111,7 +111,7 @@ const testJson = `
  * @return corresponding string of the day
  */
 function getDayStr(ind) {
-    return ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday", "Sunday"][ind-1];
+  return ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][ind - 1];
 }
 
 /**
@@ -120,7 +120,7 @@ function getDayStr(ind) {
  * @return corresponding string of the month
  */
 function getMonthStr(ind) {
-    return ["January","February","March","April","May","June", "July", "August", "September", "October", "November", "December"][ind-1];
+  return ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][ind - 1];
 }
 
 /**
@@ -130,108 +130,108 @@ function getMonthStr(ind) {
  * @author Yuelin Dai
  * @return an array containing the calendar and user array
  */
- function loadJson(jsonStrAr) {
+function loadJson(jsonStrAr) {
 
-    // load all users
-    let userList = {}; // used to store all user data
-    let userNames = new Set([]); // used to init calendar
+  // load all users
+  let userList = {}; // used to store all user data
+  let userNames = new Set([]); // used to init calendar
 
-    for(let jsonStr of jsonStrAr) { // traverse each json to collect all users
-        let jsonObj = JSON.parse(jsonStr);
+  for (let jsonStr of jsonStrAr) { // traverse each json to collect all users
+    let jsonObj = JSON.parse(jsonStr);
 
-        for(let jsonUser of jsonObj.usersList) {
-            let curUser = new User(jsonUser.firstName, jsonUser.lastName, jsonUser.username, jsonUser.password, jsonUser.profileID, jsonUser.calendarID);
-            userList[jsonUser.firstName + " " + jsonUser.lastName] = curUser; // used to store all user data
-            userNames.add(jsonUser.firstName + " " + jsonUser.lastName); // used to init calendar
-        }
+    for (let jsonUser of jsonObj.usersList) {
+      let curUser = new User(jsonUser.firstName, jsonUser.lastName, jsonUser.username, jsonUser.password, jsonUser.profileID, jsonUser.calendarID);
+      userList[jsonUser.firstName + " " + jsonUser.lastName] = curUser; // used to store all user data
+      userNames.add(jsonUser.firstName + " " + jsonUser.lastName); // used to init calendar
     }
-    
-    // store Event/Task by their startDay(excluding clocktime)
-    let dateDict = {};
-    for(let jsonStr of jsonStrAr) { // traverse each json to collect all events/tasks
-        let jsonObj = JSON.parse(jsonStr);
+  }
 
-        for(let jsonEvent of jsonObj.eventsList) {
-            let curEvent = new Event(jsonEvent.startDay, jsonEvent.endDay, jsonEvent.eventName, jsonEvent.location, jsonEvent.description);
-        
-            // create new entry in dateDict if needed
-            if(!(jsonEvent.startDay.substring(0,8) in dateDict)) {
-                dateDict[jsonEvent.startDay.substring(0,8)] = {"events":[],"tasks":[]};
-            }
-        
-            // store Event by their startDay
-            dateDict[jsonEvent.startDay.substring(0,8)].events.push(curEvent);
-        }
-        for(let jsonTask of jsonObj.tasksList) {
-            let curTask = new Task(jsonTask.taskName, jsonTask.tags, jsonTask.dueDate, jsonTask.description, jsonTask.complete);
-        
-            // create new entry in dateDict if needed
-            if(!(jsonTask.dueDate.substring(0,8) in dateDict)) {
-                dateDict[jsonTask.dueDate.substring(0,8)] = {"events":[],"tasks":[]};
-            }
-        
-            // store Task by their startDay
-            dateDict[jsonTask.dueDate.substring(0,8)].tasks.push(curTask);
-        }
+  // store Event/Task by their startDay(excluding clocktime)
+  let dateDict = {};
+  for (let jsonStr of jsonStrAr) { // traverse each json to collect all events/tasks
+    let jsonObj = JSON.parse(jsonStr);
+
+    for (let jsonEvent of jsonObj.eventsList) {
+      let curEvent = new Event(jsonEvent.startDay, jsonEvent.endDay, jsonEvent.eventName, jsonEvent.location, jsonEvent.description);
+
+      // create new entry in dateDict if needed
+      if (!(jsonEvent.startDay.substring(0, 8) in dateDict)) {
+        dateDict[jsonEvent.startDay.substring(0, 8)] = { "events": [], "tasks": [] };
+      }
+
+      // store Event by their startDay
+      dateDict[jsonEvent.startDay.substring(0, 8)].events.push(curEvent);
+    }
+    for (let jsonTask of jsonObj.tasksList) {
+      let curTask = new Task(jsonTask.taskName, jsonTask.tags, jsonTask.dueDate, jsonTask.description, jsonTask.complete);
+
+      // create new entry in dateDict if needed
+      if (!(jsonTask.dueDate.substring(0, 8) in dateDict)) {
+        dateDict[jsonTask.dueDate.substring(0, 8)] = { "events": [], "tasks": [] };
+      }
+
+      // store Task by their startDay
+      dateDict[jsonTask.dueDate.substring(0, 8)].tasks.push(curTask);
+    }
+  }
+
+  // store Day by their located year and month
+  let monthDict = {};
+  for (const [day, { events, tasks }] of Object.entries(dateDict)) {
+    let curDate = new Date(day);
+    let curDay = new Day(curDate.getDate(), getDayStr(curDate.getDay()), events, tasks);
+
+    // convert year and month to string, used below as key for monthDict
+    let yearMonthKey = String(curDate.getFullYear()) + curDate.getMonth();
+
+    // create new entry in monthDict if needed
+    if (!(yearMonthKey in monthDict)) {
+      monthDict[yearMonthKey] = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
     }
 
     // store Day by their located year and month
-    let monthDict = {};
-    for (const [day, {events, tasks}] of Object.entries(dateDict)) {
-        let curDate = new Date(day);
-        let curDay = new Day(curDate.getDate(), getDayStr(curDate.getDay()), events, tasks);
+    monthDict[yearMonthKey][curDay.currDayInt - 1] = curDay;
+  }
 
-        // convert year and month to string, used below as key for monthDict
-        let yearMonthKey = String(curDate.getFullYear())+curDate.getMonth();
+  // store Month by their located year
+  let yearDict = {};
+  for (const [yearMonthKey, days] of Object.entries(monthDict)) {
+    let curMonthVal = yearMonthKey.substring(4, 6) - (-1);
+    let curYearStr = yearMonthKey.substring(0, 4);
+    let curMonth = new Month(curMonthVal, getMonthStr(curMonthVal), days);
 
-        // create new entry in monthDict if needed
-        if(!(yearMonthKey in monthDict)) {
-            monthDict[yearMonthKey] = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
-        }
-
-        // store Day by their located year and month
-        monthDict[yearMonthKey][curDay.currDayInt-1] = curDay;
+    // create new entry in yearDict if needed
+    if (!(curYearStr in yearDict)) {
+      yearDict[curYearStr] = [null, null, null, null, null, null, null, null, null, null, null, null];
     }
 
     // store Month by their located year
-    let yearDict = {};
-    for(const [yearMonthKey, days] of Object.entries(monthDict)) {
-        let curMonthVal = yearMonthKey.substring(4, 6) - (-1);
-        let curYearStr = yearMonthKey.substring(0,4);
-        let curMonth = new Month(curMonthVal, getMonthStr(curMonthVal), days);
+    yearDict[curYearStr][curMonthVal - 1] = curMonth;
+  }
 
-        // create new entry in yearDict if needed
-        if(!(curYearStr in yearDict)) {
-            yearDict[curYearStr] = [null, null, null, null, null, null, null, null, null, null, null, null];
-        }
+  // store all 1000 years as null
+  let yearList = [];
+  for (let i = 0; i < 1000; i++) {
+    yearList.push(null);
+  }
 
-        // store Month by their located year
-        yearDict[curYearStr][curMonthVal-1] = curMonth;
-    }
+  // fill yearList with year that has events
+  for (const [yearStr, months] of Object.entries(yearDict)) {
+    let curYear = new Year(yearStr - 0, months);
+    yearList[yearStr - 2000] = curYear;
+  }
 
-    // store all 1000 years as null
-    let yearList = [];
-    for(let i = 0; i < 1000; i++) {
-        yearList.push(null);
-    }
+  let jsonObj = JSON.parse(jsonStrAr[0]); // use first json's metadata 
+  let calendar = new Calendar(jsonObj.title, jsonObj.lastUpdated, jsonObj.calendarID, yearList, Object.values(userNames));
 
-    // fill yearList with year that has events
-    for(const [yearStr, months] of Object.entries(yearDict)) {
-        let curYear = new Year(yearStr-0, months);
-        yearList[yearStr - 2000] = curYear;
-    }
-
-    let jsonObj = JSON.parse(jsonStrAr[0]); // use first json's metadata 
-    let calendar = new Calendar(jsonObj.title, jsonObj.lastUpdated, jsonObj.calendarID, yearList, Object.values(userNames));
-
-    return [calendar, Object.values(userList)];
+  return [calendar, Object.values(userList)];
 }
 
 /**
  * Temporarily deprecated, codacy doesn't accept window.URL
  * Write a json string to local drive
  * @param {String} exportJsonStr Json string that will be saved locally
- * 
+ *
  * @author Yuelin Dai
  */
 // function download (exportJsonStr) {
