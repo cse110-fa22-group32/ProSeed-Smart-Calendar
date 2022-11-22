93a89c4857087b69f503456b4a5048f2a143a34b
Steven Khaw <swkhaw8226@yahoo.com>
2022-11-21 17:30:13 -0800

    update to branch

---
+++ source/assets/scripts/load_calendar.js
@@ -0,0 +1,16 @@
+/**
+ * @author Steven Khaw
+ * @summary loads the current month calendar based on user's machine's current date
+ */
+
+// Run the init() function when the page has loaded
+window.addEventListener('DOMContentLoaded', init);
+
+// Starts the program, all function calls trace back here
+function init() {
+  
+}
+
+function loadCalendarHTML() {
+  
+}
