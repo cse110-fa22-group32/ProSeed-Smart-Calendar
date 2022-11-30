0d5a3de45a0eeeccad6d8d05e181ee561eb3f70b
yahmad3495 <yahmad@ucsd.edu>
2022-11-30 10:10:29 -0800

    Added a password/username check that checks if inputted login info exists among pre-created users

--- source/assets/scripts/login_handler.js
+++ source/assets/scripts/login_handler.js
@@ -1,4 +1,42 @@
 /**
+ * JSON string used to store user lists. 
+ * NOTE: this already is added in JsonPopulateTest.js with a "usersList" object, I can either use
+ * that one or this one created.
+ */
+const testJson = `
+  {
+    "usersList" : [
+      {
+        "firstName" : "john",
+        "lastName" : "doe",
+        "username":"random@ucsd.edu",
+        "password":"hello1",
+        "profieldID" : "123456",
+        "calenarID" : ""
+      },
+      {
+        "firstName" : "bob",
+        "lastName" : "carl",
+        "username":"hscash@gmail.com",
+        "password":"funny34Fish",
+        "profieldID" : "678232",
+        "calenarID" : ""
+      },
+      {
+        "firstName" : "harry",
+        "lastName" : "styles",
+        "username":"csEnjoyer@yahoo.com",
+        "password":"co0l4Swag",
+        "profieldID" : "342234",
+        "calenarID" : ""
+      }
+    ],
+    "eventsList" : [],
+    "tasksList" : []
+  }
+`;
+
+/**
  * @author Steven Khaw
  * @summary 
  * 
@@ -16,7 +54,7 @@ function init() {
 
 
 /**
- * @author Steven Khaw
+ * @author Steven Khaw, Younus Ahmad
  * @summary adds event listener to ensure email and password are of correct format
  */
 function ensureEmail() {
@@ -26,11 +64,30 @@ function ensureEmail() {
     const emailInputElement = document.getElementById("email-input");
     const passwordInputElement = document.getElementById("password-input");
 
-    if ((isEmail(emailInputElement.value) && isStrongPassword(passwordInputElement.value))) {
+    let authenticated = authenticateLogin(emailInputElement.value, passwordInputElement.value);
+    if ((isEmail(emailInputElement.value) && isStrongPassword(passwordInputElement.value) && authenticated)) {
       formElement.action = "middle-ground.html";
     } else {
-      alert("Please check the formatting of your email and password.");
+      alert("Incorrect username/password combination provided or incorrectly formatted");
       formElement.action = "index.html";
     }
   });
+}
+
+/**
+ * @author Younus Ahmad
+ * @param {string} email Email that user input into the email field on login page
+ * @param {*} password  Password that user input into the password field on login
+ * @returns True if there exists some user with that password combo, false otherwise
+ */
+function authenticateLogin(email, password) {
+  const userData = loadJson([testJson]);
+  const userList = userData[1];
+
+  for (let user of userList) {
+    if (email == user.username && password == user.password) {
+      return true;
+    }
+  }
+  return false;
 }
