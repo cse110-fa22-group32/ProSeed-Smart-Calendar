38fcbb3ad7a4a46c83e52701aa10aba937bc6ba7
Peng Luo <p2luo@ucsd.edu>
2022-12-04 16:14:07 -0800

    E2E test should be done

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,7 +1,9 @@
+const { Dialog } = require("puppeteer");
+
 describe('Testing Login Functionality', () => {
 
 	beforeAll(async () => {
-	  await page.goto('https://cse110-fa22-group32.github.io/cse110-fa22-group32/');
+	  await page.goto('http://127.0.0.1:5502/source/index.html');
 	});
 
 	it('Title of login page', async () => {
@@ -14,7 +16,16 @@ describe('Testing Login Functionality', () => {
 		const password = "jdlkfjalkjflkad";
 		await page.$eval('input[id=email-input]', (el,value) => el.value = value, email);
 		await page.$eval('input[id=password-input]', (el,value) => el.value = value, password);
-		await page.$eval('button[type=submit]', el => el.click());
+
+		let button = await page.$('button[type=submit]');
+
+		// Using page.on similar below to capture alert message
+		page.on('dialog', async dialog =>{
+			await button.click();
+			expect(dialog.message()).toBe("Incorrect username/password combination provided or incorrectly formatted");
+			await dialog.dismiss();
+		})
+
 		const title = await page.title();
         expect(title).toBe('ProSeed Login');
 	}, 10000);
@@ -24,14 +35,24 @@ describe('Testing Login Functionality', () => {
 		const password = "hello1";
 		await page.$eval('input[id=email-input]', (el,value) => el.value = value, email);
 		await page.$eval('input[id=password-input]', (el,value) => el.value = value, password);
+
+		// let button = await page.$('button[type=submit]');
+
+		// const [response] = await Promise.all([
+		// 	page.waitForNavigation(),
+		// 	// button.click(),
+		// 	page.click('button[type=submit]'),
+		// ]);
+
 		await page.$eval('button[type=submit]', el => el.click());
+
 		const title = await page.title();
         expect(title).toBe('ProSeed');
 
 	}, 10000);
 
 	it('Verify remember me', async () => {
-		await page.goto('https://cse110-fa22-group32.github.io/cse110-fa22-group32/');
+		await page.goto('http://127.0.0.1:5502/source/index.html');
 		const email = "random@ucsd.edu";
 		const password = "hello1";
 		await page.$eval('input[id=email-input]', (el,value) => el.value = value, email);
