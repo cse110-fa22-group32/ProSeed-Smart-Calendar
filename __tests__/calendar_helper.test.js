3408a76396b51954780792ecf7aa8370aa61768a
MLT45 <y1dai@ucsd.edu>
2022-12-05 17:33:34 -0800

    prettify tests

--- __tests__/calendar_helper.test.js
+++ __tests__/calendar_helper.test.js
@@ -1,55 +1,59 @@
-const calendar_helper = require('../source/assets/scripts/calendar_helper.js');
+const calendar_helper = require("../source/assets/scripts/calendar_helper.js");
 
-test('getIfLeapYear', () => {
-	expect(calendar_helper.getIfLeapYear(2020)).toBe(true);
-	expect(calendar_helper.getIfLeapYear(2016)).toBe(true);
-	expect(calendar_helper.getIfLeapYear(2003)).toBe(false);
-	expect(calendar_helper.getIfLeapYear(2007)).toBe(false);
+test("getIfLeapYear", () => {
+  expect(calendar_helper.getIfLeapYear(2020)).toBe(true);
+  expect(calendar_helper.getIfLeapYear(2016)).toBe(true);
+  expect(calendar_helper.getIfLeapYear(2003)).toBe(false);
+  expect(calendar_helper.getIfLeapYear(2007)).toBe(false);
 });
 
-test('getDaysInMonth', () => {
-	expect(calendar_helper.getDaysInMonth(2020,13)).toBe(31);
-	expect(calendar_helper.getDaysInMonth(2016,2)).toBe(29);
-	expect(calendar_helper.getDaysInMonth(2020,9)).toBe(30);
-	expect(calendar_helper.getDaysInMonth(2022,10)).toBe(31);
+test("getDaysInMonth", () => {
+  expect(calendar_helper.getDaysInMonth(2020, 13)).toBe(31);
+  expect(calendar_helper.getDaysInMonth(2016, 2)).toBe(29);
+  expect(calendar_helper.getDaysInMonth(2020, 9)).toBe(30);
+  expect(calendar_helper.getDaysInMonth(2022, 10)).toBe(31);
 });
 
-test('indexToMonth', () => {
-	expect(calendar_helper.indexToMonth(10)).toBe("October");
-	expect(calendar_helper.indexToMonth(11)).toBe("November");
-	expect(calendar_helper.indexToMonth(8)).toBe("August");
-	expect(calendar_helper.indexToMonth(7)).toBe("July");
+test("indexToMonth", () => {
+  expect(calendar_helper.indexToMonth(10)).toBe("October");
+  expect(calendar_helper.indexToMonth(11)).toBe("November");
+  expect(calendar_helper.indexToMonth(8)).toBe("August");
+  expect(calendar_helper.indexToMonth(7)).toBe("July");
 });
 
-test('indexToDay', () => {
-	expect(calendar_helper.indexToDay(1)).toBe("Monday");
-	expect(calendar_helper.indexToDay(2)).toBe("Tuesday");
-	expect(calendar_helper.indexToDay(5)).toBe("Friday");
-	expect(calendar_helper.indexToDay(6)).toBe("Saturday");
+test("indexToDay", () => {
+  expect(calendar_helper.indexToDay(1)).toBe("Monday");
+  expect(calendar_helper.indexToDay(2)).toBe("Tuesday");
+  expect(calendar_helper.indexToDay(5)).toBe("Friday");
+  expect(calendar_helper.indexToDay(6)).toBe("Saturday");
 });
 
-test('getWeekDayString', () => {
-	expect(calendar_helper.getWeekDayString(2021,12,1)).toBe('Wednesday');
-	expect(calendar_helper.getWeekDayString(2016,2,1)).toBe('Monday');
-	expect(calendar_helper.getWeekDayString(2022,11, 28)).toBe('Monday');
-	expect(calendar_helper.getWeekDayString(2022,12,1)).toBe('Thursday');
+test("getWeekDayString", () => {
+  expect(calendar_helper.getWeekDayString(2021, 12, 1)).toBe("Wednesday");
+  expect(calendar_helper.getWeekDayString(2016, 2, 1)).toBe("Monday");
+  expect(calendar_helper.getWeekDayString(2022, 11, 28)).toBe("Monday");
+  expect(calendar_helper.getWeekDayString(2022, 12, 1)).toBe("Thursday");
 });
 
-test('getWeekDayIndex', () => {
-	expect(calendar_helper.getWeekDayIndex(2021,12,1)).toBe(3);
-	expect(calendar_helper.getWeekDayIndex(2016,2,1)).toBe(1);
-	expect(calendar_helper.getWeekDayIndex(2022,11, 28)).toBe(1);
-	expect(calendar_helper.getWeekDayIndex(2022,12,1)).toBe(4);
+test("getWeekDayIndex", () => {
+  expect(calendar_helper.getWeekDayIndex(2021, 12, 1)).toBe(3);
+  expect(calendar_helper.getWeekDayIndex(2016, 2, 1)).toBe(1);
+  expect(calendar_helper.getWeekDayIndex(2022, 11, 28)).toBe(1);
+  expect(calendar_helper.getWeekDayIndex(2022, 12, 1)).toBe(4);
 });
 
-test('getWeekCount', () => {
-	expect(calendar_helper.getWeekCount(2021,12)).toBe(5);
-	expect(calendar_helper.getWeekCount(2021,2)).toBe(5);
-	expect(calendar_helper.getWeekCount(2014,2)).toBe(5);
-	expect(calendar_helper.getWeekCount(2022,12)).toBe(5);
+test("getWeekCount", () => {
+  expect(calendar_helper.getWeekCount(2021, 12)).toBe(5);
+  expect(calendar_helper.getWeekCount(2021, 2)).toBe(5);
+  expect(calendar_helper.getWeekCount(2014, 2)).toBe(5);
+  expect(calendar_helper.getWeekCount(2022, 12)).toBe(5);
 });
 
-test('getCurrentDay', () => {
-	let today = new Date();
-	expect(calendar_helper.getCurrentDay()).toStrictEqual([`${today.getFullYear()}` ,`${today.getMonth() + 1}`,`${today.getDate()}`]);
+test("getCurrentDay", () => {
+  let today = new Date();
+  expect(calendar_helper.getCurrentDay()).toStrictEqual([
+    `${today.getFullYear()}`,
+    `${today.getMonth() + 1}`,
+    `${today.getDate()}`,
+  ]);
 });
--- __tests__/json_interpreter.test.js
+++ __tests__/json_interpreter.test.js
@@ -1,5 +1,4 @@
-
-let {loadJson} = require('../source/assets/scripts/json_interpreter.js')
+let { loadJson } = require("../source/assets/scripts/json_interpreter.js");
 
 const json1 = `
  {
@@ -40,22 +39,30 @@ const json1 = `
    }
  `;
 
-test('loadJson', () => {
-	let calData = loadJson([json1]);
+test("loadJson", () => {
+  let calData = loadJson([json1]);
 
-  expect(calData[0].lastUpdated).toBe('');
-  expect(calData[0].title).toBe('title1');
-  expect(calData[0].calendarID).toBe('');
+  expect(calData[0].lastUpdated).toBe("");
+  expect(calData[0].title).toBe("title1");
+  expect(calData[0].calendarID).toBe("");
   expect(calData[1].length).toBe(1);
 });
 
-test('Export', () => {
-	let calData = loadJson([json1]);
+test("Export", () => {
+  let calData = loadJson([json1]);
 
-	// console.log(cal.Export([-1], [-1], [-1]));
-	// console.log(JSON.stringify(JSON.parse(json1)));
-  expect(calData[0].Export([-1], [-1], [-1])).toBe(JSON.stringify(JSON.parse(json1)));
-  expect(calData[0].Export([-1], [-1], [-1])).toBe(calData[0].Export([2022],[-1],[-1]));
-  expect(calData[0].Export([-1], [-1], [-1])).toBe(calData[0].Export([-1],[12],[-1]));
-  expect(calData[0].Export([-1], [-1], [-1])).toBe(calData[0].Export([-1],[-1],[1]));
+  // console.log(cal.Export([-1], [-1], [-1]));
+  // console.log(JSON.stringify(JSON.parse(json1)));
+  expect(calData[0].Export([-1], [-1], [-1])).toBe(
+    JSON.stringify(JSON.parse(json1))
+  );
+  expect(calData[0].Export([-1], [-1], [-1])).toBe(
+    calData[0].Export([2022], [-1], [-1])
+  );
+  expect(calData[0].Export([-1], [-1], [-1])).toBe(
+    calData[0].Export([-1], [12], [-1])
+  );
+  expect(calData[0].Export([-1], [-1], [-1])).toBe(
+    calData[0].Export([-1], [-1], [1])
+  );
 });
